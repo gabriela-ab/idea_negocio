670d7427bb362d6c5a80cb258a0ea6b4a5a9a95c
gabriela-ab <74192983+gabriela-ab@users.noreply.github.com>
2021-03-18 15:12:53 -0500

    estilos para el index

--- index.php
+++ index.php
@@ -8,11 +8,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/estilos.css">
 </head>
 <body>
     <h1>Automatizacion en la veterinaria</h1>
     <p>si funciona o que carajos</p>
     <a href="#">continuemos</a>
+    <a href="#">listo</a>
     
 </body>
 </html>
