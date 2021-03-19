56b86ec70e10ea7da5698aa530e421af86b4ad54
gabriela-ab <74192983+gabriela-ab@users.noreply.github.com>
2021-03-19 18:14:36 -0500

    conexion con la base de datos

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,3 +1,14 @@
 <?php
+$hostname="localhost";
+$username="root";
+$password= "";
+$database="veterinaria";
+$conexion = new mysqli($hostname,$username,$password,$database);
+
+if($conexion ->connect_errno)
+{
+  die("fallo la conexion con la base de datos $database" . mysqli_connect_errno());
+}
+session_start();
 
 ?>
