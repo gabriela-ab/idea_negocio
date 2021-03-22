d08c0bb515f259ab67ee40afe2aacece13fa2470
gabriela-ab <74192983+gabriela-ab@users.noreply.github.com>
2021-03-22 18:23:57 -0500

    estilos para formulario de registro

--- regis.php
+++ regis.php
@@ -24,43 +24,46 @@ require_once('conexion/conexion.php');
 </head>
 <body>
 
-    <div>
-        <form action="php/registro.php" method="POST">
+    <div class="formulario">
+        <form action="php/registro.php" method="POST" autocomplete="off">
             <img src="img/veterinario.png" alt="">
-            <h1 class="login">Registrarse</h1>
-            <select name="tipo_docu" id="tipo_docu" required>
-                <option value="">Elije Tipo Documento</option>
-                <?php
+            <h1 class="titulo">Registrarse</h1>
+            <div class="regis">
+                <select name="tipo_docu" id="tipo_docu" required>
+                    <option value="">Elije tipo documento</option>
+                    <?php
 
-                    foreach ($query1 as $tip_docu) : ?>
+                        foreach ($query1 as $tip_docu) : ?>
 
-                <option value="<?php echo $tip_docu['id_tip_docu'] ?> ">
-                    <?php echo $tip_docu['tipo_documento'] ?></option>
-                <?php
-                endforeach;
-                ?>
-            </select>
-            <input type="text" name="documento" id="documento" placeholder="Digite su Documento" required>
-            <input type="text" name="nombres" id="nombres" placeholder="Digite su Nombres" required>
-            <input type="text" name="apellidos" id="apellidos" placeholder="Digite su Apellidos" required>
-            <input type="text" name="usuario" id="usuario" placeholder="Digite su Usuario" required>
-            <input type="text" name="clave" id="clave" placeholder="Digite su Contraseña" required>
-            <input type="text" name="telefono" id="telefono" placeholder="Digite su Telefono" required>
-            <input type="text" name="email" id="email" placeholder="Digite su Email" required>
+                    <option value="<?php echo $tip_docu['id_tip_docu'] ?> ">
+                        <?php echo $tip_docu['tipo_documento'] ?></option>
+                    <?php
+                    endforeach;
+                    ?>
+                </select>
+                <input type="text" name="documento" id="documento" class="docu" placeholder="Digite su documento" required>
+                <input type="text" name="nombres" id="nombres" placeholder="Digite su nombres" required>
+                <input type="text" name="apellidos" id="apellidos" placeholder="Digite su apellidos" required>
+                <input type="text" name="usuario" id="usuario" placeholder="Digite su usuario" required>
+                <input type="text" name="clave" id="clave" placeholder="Digite su contraseña" required>
+                <input type="text" name="telefono" id="telefono" placeholder="Digite su telefono" required>
+                <input type="text" name="email" id="email" placeholder="Digite su email" required>
+                
+                <select name="tipo_usu" id="tipo_usu" required>
+                    <option value="">Elije tipo usuario</option>
+                    <?php
+
+                        foreach ($query2 as $tip_usu) : ?>
+
+                    <option value="<?php echo $tip_usu['id_tip_usu'] ?> ">
+                        <?php echo $tip_usu['tipo_usuario'] ?></option>
+                    <?php
+                    endforeach;
+                    ?>
+                </select>
+                <input type="submit" class="enviar" name="enviar" Value="Registrar" id="enviar">
+            </div>
             
-            <select name="tipo_usu" id="tipo_usu" required>
-                <option value="">Elije Tipo Usuario</option>
-                <?php
-
-                    foreach ($query2 as $tip_usu) : ?>
-
-                <option value="<?php echo $tip_usu['id_tip_usu'] ?> ">
-                    <?php echo $tip_usu['tipo_usuario'] ?></option>
-                <?php
-                endforeach;
-                ?>
-            </select>
-            <input type="submit" name="enviar" Value="Registrar" id="enviar">
 
         </form>
     </div>
