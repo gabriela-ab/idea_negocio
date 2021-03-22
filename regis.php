6e41ad4953ee2731c2b59169e43ef6ab157a1eaf
gabriela-ab <74192983+gabriela-ab@users.noreply.github.com>
2021-03-22 15:48:41 -0500

    creacion del formulario de registro

---
+++ regis.php
@@ -0,0 +1,36 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrarse</title>
+    <link rel="stylesheet" href="css/regis.css">
+</head>
+<body>
+
+    <div>
+        <form action="" method="post">
+            <img src="img/veterinario.png" alt="">
+            <h1 class="login">Registrarse</h1>
+            <input type="text" name="documento" id="documento" placeholder="Digite su Documento" required>
+            <input type="text" name="nombres" id="nombres" placeholder="Digite su Nombres" required>
+            <input type="text" name="apellidos" id="apellidos" placeholder="Digite su Apellidos" required>
+            <input type="text" name="usuario" id="usuario" placeholder="Digite su Usuario" required>
+            <input type="text" name="clave" id="clave" placeholder="Digite su Contraseña" required>
+            <input type="text" name="telefono" id="telefono" placeholder="Digite su Telefono" required>
+            <input type="text" name="email" id="email" placeholder="Digite su Email" required>
+            <select name="tipo_doc" id="tipo_doc" required>
+
+            </select>
+            <input type="text" name="" id="" placeholder="Digite su Documento" required>
+            <input type="submit" name="enviar" Value="Registrar" id="enviar">
+
+        </form>
+    </div>
+    
+</body>
+</html>
