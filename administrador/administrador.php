1ff693120939f1759afc0c9072dc27496e37cab0
gabriela-ab <74192983+gabriela-ab@users.noreply.github.com>
2021-03-22 19:40:34 -0500

    direccionando archivos css para admin y usuario

--- administrador/administrador.php
+++ administrador/administrador.php
@@ -12,7 +12,7 @@ echo('TODO ESTA BIEN');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="shortcut icon" href="" type="image/x-icon">
-    <link rel="stylesheet" href="css/stylo_admin.css">
+    <link rel="stylesheet" href="../css/admin.css">
 </head>
 <body>
 <nav >
--- usuario/usuario.php
+++ usuario/usuario.php
@@ -12,7 +12,7 @@ echo('TODO ESTA BIEN');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="shortcut icon" href="img/fondo1.jpg" type="image/x-icon">
-    <link rel="stylesheet" href="css/stilo_usuar.css">
+    <link rel="stylesheet" href="../css/usu.css">
 </head>
 <body>
 <nav >
