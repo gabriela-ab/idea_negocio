11377b4d5b3626999fbf41632bcd38ead31e5d1b
gabriela-ab <74192983+gabriela-ab@users.noreply.github.com>
2021-03-23 15:52:00 -0500

    creacion de archivo del control

--- administrador/control.php
+++ administrador/control.php
@@ -2,6 +2,11 @@
 require_once('../conexion/conexion.php');
 
 ?>
+<?php
+    $sqlcon = "SELECT * FROM tipo_control";
+    $quercon = mysqli_query($conexion,$sqlcon);
+    $filacon = mysqli_fetch_assoc($querycon);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,7 +18,20 @@ require_once('../conexion/conexion.php');
 <body>
     <div class="formul">
         <form action="" method="POST">
-            <input type="text" name="">
+            <select name="tipo_docu" id="tipo_docu" required>
+                <option value="">Elije tipo documento</option>
+                <?php
+
+                    foreach ($querycon as $tip_con) : ?>
+
+                <option value="<?php echo $tip_con['id_tip_control'] ?> ">
+                    <?php echo $tip_con['tipo_control'] ?></option>
+                <?php
+                endforeach;
+                ?>
+            </select>
+
+            <input type="text" name="control">
         </form>
     </div>
     
