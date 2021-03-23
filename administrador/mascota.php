46485e998f3dc07d852f4d6d0096130333e19069
gabriela-ab <74192983+gabriela-ab@users.noreply.github.com>
2021-03-22 21:01:47 -0500

    creacion de formulario de registro de mascota

--- administrador/mascota.php
+++ administrador/mascota.php
@@ -2,6 +2,20 @@
 require_once('../conexion/conexion.php');
 
 ?>
+<?php
+
+    $sqlmas = "SELECT * FROM tipo_mascota";
+    $querymas = mysqli_query($conexion,$sqlmas);
+    $filamas = mysqli_fetch_assoc($querymas);
+?>
+
+<?php
+
+    $sqlge = "SELECT * FROM genero";
+    $queryge = mysqli_query($conexion,$sqlge);
+    $filage = mysqli_fetch_assoc($queryge);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,7 +29,33 @@ require_once('../conexion/conexion.php');
     <div class="formulario">
         <form action="" method="POST">
             <input type="number" name="codigo" id="codigo" placeholder="Digita codigo mascota" required>
-            <input type="number" name="codigo" id="codigo" placeholder="Digita codigo mascota" required>
+            <input type="text" name="nombre" id="nombre" placeholder="Digita el nombre" required>
+            <select name="tipo_mas" id="tipo_mas" required>
+                <option value="">Elije tipo mascota</option>
+                <?php
+
+                    foreach ($querymas as $tip_mas) : ?>
+
+                <option value="<?php echo $tip_mas['id_tip_mascota'] ?> ">
+                    <?php echo $tip_mas['tipo_mascota'] ?></option>
+                <?php
+                endforeach;
+                ?>
+            </select>
+            <input type="text" name="raza" id="raza" placeholder="Digita la raza" required>
+            <input type="text" name="edad" id="edad" placeholder="Digita la edad" required>
+            <select name="genero" id="genero" required>
+                <option value="">Elije genero</option>
+                <?php
+
+                    foreach ($queryge as $genero) : ?>
+
+                <option value="<?php echo $genero['id_genero'] ?> ">
+                    <?php echo $genero['genero'] ?></option>
+                <?php
+                endforeach;
+                ?>
+            </select>
 
         </form>
     </div>
