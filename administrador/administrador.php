a278d230486fda84ff5508a071dcf8e3cba18378
gabriela-ab <74192983+gabriela-ab@users.noreply.github.com>
2021-03-23 09:45:43 -0500

    terminando los ajustes de css

--- administrador/administrador.php
+++ administrador/administrador.php
@@ -9,31 +9,31 @@ require_once('../conexion/conexion.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu | Administrador</title>
-    
     <link rel="stylesheet" href="../css/stylo_admin.css">
     
 </head>
 <body>
-<div class="container1">
-                <h1> </h1>
-                <h2>ADMINISTRADOR</h2>
-            <figure>
-                <img src="../img/veterinario.png" alt="logo de la pagina">
-            </figure>
-</div>
+    <div class="container1">
+        <h1> </h1>
+        <h2>ADMINISTRADOR</h2>
+        <figure>
+            <img src="../img/veterinario.png" alt="logo de la pagina">
+        </figure>
+    </div>
     <div>
-                    <div class="menu">
-                        <ul >
-                            <li><a href="administrador.php"></a>INICIO</a></li>
-                            <li><a href="">CONTROL</a></li>
-                            <li><a href="">MASCOTAS</a></li>
-                            <li><a href="">USUARIOS</a></li>
-                            <li><a href="">INVENTARIO</a></li>
-                            <li><a href="../php/salir.php">salir</a></li>
-                        </ul>
+        <div class="menu">
+            <ul>
+                <li><a href="administrador.php"></a>INICIO</a></li>
+                <li><a href="">CONTROL</a></li>
+                <li><a href="">MASCOTAS</a></li>
+                <li><a href="">USUARIOS</a></li>
+                <li><a href="">INVENTARIO</a></li>
+                <li><a href="../php/salir.php"><img witdh="40px" height="50px" src="../img/cerrar-sesion.png" alt=""></a></li>
+            </ul>
                        
-                    </div>
-     </div>
+        </div>
+    </div>
+
    <h1 class="saludo">BIENVENIDO</h1>
   
 </body>
