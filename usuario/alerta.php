f984d6129186055586454338df8bbaf6d9c695a0
gabriela-ab <74192983+gabriela-ab@users.noreply.github.com>
2021-03-24 14:49:43 -0500

    creando archivo de la alerta para el usuario

--- usuario/alerta.php
+++ usuario/alerta.php
@@ -5,8 +5,25 @@ require_once('../conexion/conexion.php');
     $consulta_usu = "SELECT * FROM usuario, tipo_usuario WHERE usuario = '".$_SESSION['usuario']."' and usuario.id_tip_usu = tipo_usuario.id_tip_usu";
     $queryusu = mysqli_query($conexion,$consulta_usu);
     $filausu = mysqli_fetch_assoc($queryusu);
-    $_SESSION['documento'] = $filausu['documento'];
+    $_SESSION['documento'] = $filausu['documento'];    
 ?>
+
+<?php 
+    $consulta_doc = "SELECT * FROM mascota, control WHERE mascota.codigo = control.codigo";
+    $querydocu = mysqli_query($conexion,$consulta_doc);
+    $filadocu = mysqli_fetch_assoc($querydocu);
+?>
+
+<?php
+    $consulta_mas = "SELECT * FROM control where codigo = ";
+    $querymas = mysqli_query($conexion,$consulta_mas);
+    $filamas= mysqli_fetch_assoc($querymas);
+
+?>
+
+
+
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -17,10 +34,12 @@ require_once('../conexion/conexion.php');
     <title>Mensaje alerta</title>
 </head>
 <body>
-    <script>
-        alert("hola")
+    <p><?php echo $filausu['documento'] ?></p>
+    <p><?php echo $filadocu['documento'] ?></p>
+    <p><?php echo $filamas['codigo'] ?></p>
 
-    </script>
+    
+
     
 </body>
 </html>
